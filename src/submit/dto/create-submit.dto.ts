9dd22c4b3eb9420bd89e5f9bb1b2883171668d6c
hun1i <nicebrian0328@gmail.com>
2024-05-16 20:14:16 +0900

    ADD :: create dto 설정

--- src/submit/dto/create-submit.dto.ts
+++ src/submit/dto/create-submit.dto.ts
@@ -1,1 +1,42 @@
-export class CreateSubmitDto {}
+import {
+  IsEmail,
+  IsEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateSubmitDto {
+  @IsString()
+  programName: string;
+
+  //@IsEnum(TimeType) 왜 안되지..
+  @IsNotEmpty()
+  time: string;
+
+  @IsString()
+  date: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  name: string;
+
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsString()
+  organization: string;
+
+  @IsInt()
+  participant: number;
+
+  @IsInt()
+  grade: number;
+
+  @IsEmpty()
+  @IsString()
+  question: string;
+}
