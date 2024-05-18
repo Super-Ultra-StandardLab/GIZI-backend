5c2825c5f7b3efe3c83e2e071f1ddc2158fb2f21
hun1i <nicebrian0328@gmail.com>
2024-05-18 21:20:51 +0900

    CHORE :: 할 것 주석 추가

--- src/submit/submit.service.ts
+++ src/submit/submit.service.ts
@@ -20,12 +20,12 @@ export class SubmitService {
     });
 
     if (createSubmitDto.time === 'allday' && checkDate.length !== 0) {
-      throw new ConflictException('선택하신 시간에 이미 신청자가 존재합니다.');
+      throw new ConflictException('선택하신 시간에 이미 신청자가 존재합니다.'); // 나중에 한번에 customException으로 처리하기
     }
     checkDate.forEach((item) => {
       if (item.time === createSubmitDto.time || item.time === 'allday') {
         throw new ConflictException(
-          `선택하신 시간에 이미 신청자가 존재합니다.`,
+          `선택하신 시간에 이미 신청자가 존재합니다.`, // 나중에 한번에 customException으로 처리하기
         );
       }
     });
