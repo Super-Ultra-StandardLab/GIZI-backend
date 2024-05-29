4d95a342dcc09c419b5194bfa82a49f4002d9b5a
hun1i <nicebrian0328@gmail.com>
2024-05-29 16:52:24 +0900

    REFACTOR :: dateEntity로 따로 만들기

--- src/verify/function/validateDate.ts
+++ src/verify/function/validateDate.ts
@@ -7,7 +7,7 @@ export function validateDate(checkDate, createSubmitDto) {
   checkDate.forEach((item) => {
     if (item.time === createSubmitDto.time || item.time === 'allday') {
       throw new ConflictException(
-        `선택하신 시간에 이미 신청자가 존재합니다.`, // FIXME: 나중에 한번에 customException으로 처리하기
+        '선택하신 시간에 이미 신청자가 존재합니다.', // FIXME: 나중에 한번에 customException으로 처리하기
       );
     }
   });
