182d2b4af789a8774521832e7518436410f1664e
hun1i <nicebrian0328@gmail.com>
2024-05-29 14:31:37 +0900

    REFACTOR :: secure random 로직 적용

--- src/submit/submit.service.ts
+++ src/submit/submit.service.ts
@@ -20,12 +20,12 @@ export class SubmitService {
     });
 
     if (createSubmitDto.time === 'allday' && checkDate.length !== 0) {
-      throw new ConflictException('선택하신 시간에 이미 신청자가 존재합니다.'); // 나중에 한번에 customException으로 처리하기
+      throw new ConflictException('선택하신 시간에 이미 신청자가 존재합니다.'); // FIXME: 나중에 한번에 customException으로 처리하기
     }
     checkDate.forEach((item) => {
       if (item.time === createSubmitDto.time || item.time === 'allday') {
         throw new ConflictException(
-          `선택하신 시간에 이미 신청자가 존재합니다.`, // 나중에 한번에 customException으로 처리하기
+          `선택하신 시간에 이미 신청자가 존재합니다.`, // FIXME: 나중에 한번에 customException으로 처리하기
         );
       }
     });
@@ -34,7 +34,13 @@ export class SubmitService {
   }
 
   async findAll(): Promise<Submit[]> {
-    return ResponseAllSubmitDto.listOf(await this.SubmitRepository.find());
+    return ResponseAllSubmitDto.listOf(
+      await this.SubmitRepository.find({
+        order: {
+          date: 'DESC',
+        },
+      }),
+    );
   }
 
   async findOne(programId: bigint): Promise<ResponseSubmitDto> {
